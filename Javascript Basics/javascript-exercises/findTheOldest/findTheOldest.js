f27b1da749a0a85d678d581427c155ac4e314537
larsfor <lars.forse@gail.com>
2021-03-28 22:52:02 +0200

    Finished getTheTitles and findTheOldest

--- Javascript Basics/javascript-exercises/findTheOldest/findTheOldest.js
+++ Javascript Basics/javascript-exercises/findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(array) {
+    let age = 0;
+    let yearOfDeath = 0;
+    const oldest = array.reduce(function(obj, item) {
+        
+        // If person isn't dead yet, use current year
+        if (!item.yearOfDeath) {
+            yearOfDeath = new Date().getFullYear();
+        } else {
+            yearOfDeath = item.yearOfDeath;
+        }
+        
+        // If the current persons age > largest age so far, replace the person
+        if (yearOfDeath - item.yearOfBirth > age) {
+            age = yearOfDeath - item.yearOfBirth;
+            obj["name"] = item.name;
+        }
+        return obj;
+    }, {})
+    return oldest;
 }
-
 module.exports = findTheOldest
--- Javascript Basics/javascript-exercises/getTheTitles/getTheTitles.js
+++ Javascript Basics/javascript-exercises/getTheTitles/getTheTitles.js
@@ -1,5 +1,5 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(object) {
+    return object.map(book => book.title);
 }
 
 module.exports = getTheTitles;
