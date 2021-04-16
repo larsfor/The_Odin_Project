bbbb1f00958e4a06e00db9997992434c81398cb1
larsfor <lars.forse@gail.com>
2021-04-16 17:55:59 +0200

    Finished Project: Stock Picker

---
+++ Ruby on Rails/Basic Ruby Projects/Project: Stock Picker/stockpicker.rb
@@ -0,0 +1,24 @@
+def stock_picker(array)
+  result = 0
+  best_days = []
+
+  # Loop through each of the days checking the profit for each proceeding day minus current day
+  # If the future day minus the current day is larger than current result, we replace the result
+  # and the indices of the array best_days
+  array.each_with_index {|day, day_index|
+    array.each_with_index {|profit, profit_index|
+      if profit_index > day_index
+        if array[profit_index] - array[day_index] > result
+          result = array[profit_index] - array[day_index]
+          best_days[0] = day_index
+          best_days[1] = profit_index
+         end
+      end
+    }
+  }
+
+  p "#{best_days} for a profit of $#{array[best_days[1]]} - $#{array[best_days[0]]} = #{result}"
+
+end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
