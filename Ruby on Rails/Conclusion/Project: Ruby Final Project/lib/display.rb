8798cba097083927ef2934d5c01803f1786b8e52
larsfor <lars.forse@gail.com>
2021-08-13 14:36:49 +0200

    Finished Display class Heavily inspired by R.L Moser's TicTacToe

--- Ruby on Rails/Conclusion/Project: Ruby Final Project/lib/display.rb
+++ Ruby on Rails/Conclusion/Project: Ruby Final Project/lib/display.rb
@@ -1,1 +1,36 @@
 # frozen_string_literal: true
+
+# Text needed to play chess
+module Display
+  def display_intro
+    "Let's play a game of chess in the console! \n\n"
+  end
+
+  def display_name_promt(number)
+    "What is the name of player ##{number}?"
+  end
+
+  def display_color_prompt
+    'Which color do you want to play with?'
+  end
+
+  def display_first_color(duplicate)
+    "It can't be '#{duplicate}'"
+  end
+
+  def display_input_warning
+    "\e[31mSorry, that is an invalid answer. Please, try again.\e[0m"
+  end
+
+  def display_player_turn(name)
+    "#{name}, please pick a position on the board that is available for the pirce to place on."
+  end
+
+  def display_winner(player)
+    "GAME OVER! #{player} is the winner!"
+  end
+
+  def display_tie
+    "It's a draw."
+  end
+end
