1c5af307771dbc0de2728221de710fd7d32be959
larsfor <lars.forse@gail.com>
2021-08-13 15:31:32 +0200

    Player class Finished Player class, heavily inspired by R.L. Moser's TicTacToe

--- Ruby on Rails/Conclusion/Project: Ruby Final Project/lib/player.rb
+++ Ruby on Rails/Conclusion/Project: Ruby Final Project/lib/player.rb
@@ -1,1 +1,11 @@
 # frozen_string_literal: true
+
+# The game need two players
+class Player
+  attr_reader :name, :symbol
+
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
+  end
+end
