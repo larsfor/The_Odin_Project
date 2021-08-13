b137dc0836bae5f2361ceedead83cd8055aaab63
larsfor <lars.forse@gail.com>
2021-08-13 13:24:35 +0200

    Pawn and Main Fixed some moves-logic in Pawn and added logic to main.rb

--- Ruby on Rails/Conclusion/Project: Ruby Final Project/lib/pawn.rb
+++ Ruby on Rails/Conclusion/Project: Ruby Final Project/lib/pawn.rb
@@ -2,22 +2,24 @@
 
 # The logic behind the pawn chess piece
 class Pawn
-  attr_reader :all_moves, :color, :symbol
-  attr_accessor :position
+  attr_reader :all_moves, :color, :symbol, :starting_moves
+  attr_accessor :position, :first_move
 
   def initialize(color, position)
     @position = position
     @color = color
     @symbol = choose_piece_color
+    @first_move = true
+    @starting_moves = [
+      [-2, 0], [2, 0]
+    ]
     @all_moves = [
       [-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]
     ]
   end
 
   def possible_moves
-    all_moves.each do |moves|
-      p moves
-    end
+    first_move == true ? starting_moves : all_moves
   end
 
   def choose_piece_color
