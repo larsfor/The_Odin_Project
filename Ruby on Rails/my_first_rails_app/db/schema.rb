fa4cd735544efc0e8e5249f5c4d09c15a1cc2d76
larsfor <lars.forse@gmail.com>
2021-12-26 18:22:20 +0100

    Initial commit

--- Ruby on Rails/my_first_rails_app/db/schema.rb
+++ Ruby on Rails/my_first_rails_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_26_163106) do
+ActiveRecord::Schema.define(version: 2021_12_26_172124) do
 
   create_table "cars", force: :cascade do |t|
     t.string "make"
