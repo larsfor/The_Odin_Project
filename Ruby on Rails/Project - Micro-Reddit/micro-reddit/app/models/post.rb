42450680e4338e8a574a2bab74703544aa9ee807
Lars Fors <lars.forse@gmail.com>
2022-05-29 16:54:16 +0200

    Made some posts and tried out some logic

--- Ruby on Rails/Project - Micro-Reddit/micro-reddit/app/models/post.rb
+++ Ruby on Rails/Project - Micro-Reddit/micro-reddit/app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true, length: {minimum: 5}
+  validates :user_id, presence: true, length: {minimum: 1}
 
   belongs_to :user
 end
