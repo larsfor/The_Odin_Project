3db226d3e48214a8e8c1383ffed987d3bddfcc3d
Lars Fors <lars.forse@gmail.com>
2022-05-29 16:07:17 +0200

    added validations for User model

--- Ruby on Rails/Project - Micro-Reddit/micro-reddit/app/models/user.rb
+++ Ruby on Rails/Project - Micro-Reddit/micro-reddit/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true, length: {minimum: 5}
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: {minimum: 10}
 end
