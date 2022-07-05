4f87e41f7b05781028372449f103382ddb68655b
Lars Fors <lars.forse@gmail.com>
2022-07-05 22:18:07 +0200

    Finished the logic - new, create, index and showing author if you're signed in.

--- Ruby on Rails/Project - Members only!/Members only!/members-only/config/routes.rb
+++ Ruby on Rails/Project - Members only!/Members only!/members-only/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'posts/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :posts, only: [:new, :create, :edit, :update]
+  resources :posts, only: [:new, :create, :index]
 
   # Defines the root path route ("/")
   # root "articles#index"
