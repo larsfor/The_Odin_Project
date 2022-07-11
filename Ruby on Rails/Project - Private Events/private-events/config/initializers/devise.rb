68c044302bf179fc1c76ad525a07d8f6564fe363
Lars Fors <lars.forse@gmail.com>
2022-07-11 18:13:25 +0200

    Fixed the TurboController

--- Ruby on Rails/Project - Private Events/private-events/config/initializers/devise.rb
+++ Ruby on Rails/Project - Private Events/private-events/config/initializers/devise.rb
@@ -1,23 +1,17 @@
 # frozen_string_literal: true
-class TurboController < ApplicationController
-  class Responder < ActionController::Responder
-    def to_turbo_stream
-      controller.render(options.merge(formats: :html))
-    rescue ActionView::MissingTemplate => error
-      if get?
-        raise error
-      elsif has_errors? && default_action
-        render rendering_options.merge(formats: :html, status: :unprocessable_entity)
-      else
-        redirect_to navigation_location
-      end
+class TurboFailureApp < Devise::FailureApp
+  def respond
+    if request_format == :turbo_stream
+      redirect
+    else
+      super
     end
   end
 
-  self.responder = Responder
-  respond_to :html, :turbo_stream
+  def skip_format?
+    %w(html turbo_stream */*).include? request_format.to_s
+  end
 end
-
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
 # are not: uncommented lines are intended to protect your configuration from
