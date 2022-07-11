6ac49400c78a9b0a558e0ad076ad7325391dae6d
Lars Fors <lars.forse@gmail.com>
2022-07-11 18:46:26 +0200

    Removed Devise

--- Ruby on Rails/Project - Private Events/private-events/app/controllers/events_controller.rb
+++ Ruby on Rails/Project - Private Events/private-events/app/controllers/events_controller.rb
@@ -2,6 +2,6 @@ class EventsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @event = Event.all.order("created_at DESC")
+    @events = Event.all.order("created_at DESC")
   end
 end
