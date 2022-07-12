583c9f245c7fa5b6d3be41c29fbba59608dfd0b5
Lars Fors <lars.forse@gmail.com>
2022-07-12 20:08:37 +0200

    Fixed some layout and user logic

--- Ruby on Rails/Project - Private Events/private-events/config/routes.rb
+++ Ruby on Rails/Project - Private Events/private-events/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'users/index'
-  devise_for :users
+  devise_for :users, :controllers => { registrations: "registrations" }
   resources :events
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- Ruby on Rails/Project - Private Events/private-events/db/schema.rb
+++ Ruby on Rails/Project - Private Events/private-events/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_12_173318) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_12_174654) do
   create_table "events", force: :cascade do |t|
     t.datetime "date"
     t.string "location"
@@ -28,8 +28,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_12_173318) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
