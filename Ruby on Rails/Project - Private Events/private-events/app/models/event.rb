1254bdb1e5111b87188c3a2cee8ee5d7401ca446
Lars Fors <lars.forse@gmail.com>
2022-07-14 00:49:58 +0200

    Added two class methods on the Even model (old way)

--- Ruby on Rails/Project - Private Events/private-events/app/models/event.rb
+++ Ruby on Rails/Project - Private Events/private-events/app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
     has_many :event_attendings, foreign_key: :attended_event_id
     has_many :attendees, through: :event_attendings, source: :event_attendee
     belongs_to :creator, class_name: "User"
+
+    def self.past
+        Event.where("date <= ?", Time.now)
+    end
+
+    def self.future
+        Event.where("date >= ?", Time.now)
+    end
 end
