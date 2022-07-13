543430dedaaa7ef820c81cd244ad5aeb85d21dc1
Lars Fors <lars.forse@gmail.com>
2022-07-14 00:53:11 +0200

    Refactored the class methods into scopes

--- Ruby on Rails/Project - Private Events/private-events/app/models/event.rb
+++ Ruby on Rails/Project - Private Events/private-events/app/models/event.rb
@@ -3,11 +3,16 @@ class Event < ApplicationRecord
     has_many :attendees, through: :event_attendings, source: :event_attendee
     belongs_to :creator, class_name: "User"
 
-    def self.past
-        Event.where("date <= ?", Time.now)
-    end
+    #def self.past
+    #    Event.where("date <= ?", Time.now)
+    #end
 
-    def self.future
-        Event.where("date >= ?", Time.now)
-    end
+    scope :past, -> {where("date <= ?", Time.now)}
+
+
+    #def self.future
+    #    Event.where("date >= ?", Time.now)
+    #end
+
+    scope :future, -> {where("date >= ?", Time.now)}
 end
