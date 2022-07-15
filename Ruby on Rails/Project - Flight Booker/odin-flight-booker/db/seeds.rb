6184e65c60209b016074211642f59ef50a7c5a47
Lars Fors <lars.forse@gmail.com>
2022-07-15 19:11:50 +0200

    Added Airport and Flight models

--- Ruby on Rails/Project - Flight Booker/odin-flight-booker/db/seeds.rb
+++ Ruby on Rails/Project - Flight Booker/odin-flight-booker/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Airport.create(airport_code: "SFO")
+Airport.create(airport_code: "NYC")
+Airport.create(airport_code: "CLE")
+Airport.create(airport_code: "DSM")
+Airport.create(airport_code: "FNT")
