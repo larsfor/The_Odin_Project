07747609f3705abfadd6946b9daf20ba7e35cdc5
Lars Fors <lars.forse@gmail.com>
2022-08-18 21:47:37 +0200

    Added name to the user and some logic to which actions one can make

--- Ruby on Rails/Project - Building Facebook/odin-facebook/db/seeds.rb
+++ Ruby on Rails/Project - Building Facebook/odin-facebook/db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(email: 'humblebragger@humblebrag.com', password: 'humblebaby')
-User.create(email: 'fitnessgrampacer@test.com', password: 'pacertest')
+User.create(email: 'humblebragger@humblebrag.com', password: 'humblebaby', name: 'HumbleBragger')
+User.create(email: 'fitnessgrampacer@test.com', password: 'pacertest', name: 'FitnessGrampacer')
