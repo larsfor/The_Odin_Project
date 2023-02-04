88e5c071850603310fe876ac222828e8a56963a7
Lars Fors <lars.forse@gmail.com>
2023-02-04 20:52:21 +0100

    added some functionality to show table with js

--- JavaScript/Project - Tic Tac Toe/script.js
+++ JavaScript/Project - Tic Tac Toe/script.js
@@ -2,3 +2,25 @@ window.addEventListener("DOMContentLoaded", (event) => {
     console.log('DOMContent loaded')
 });
 
+// A player constructor as a factory
+const Player = (name, marker) => {
+    return { name, marker };
+  };
+
+
+// A 3x3 array as a module
+const Gameboard = ( () => {
+    board = Array.from(Array(3), () => new Array(3, ''))
+    return { board }
+})();
+
+function generateTable() {
+    gameboard = Gameboard;
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            let GameCell = gameboard.board[i][j];
+            let HTMLcell = document.querySelector(`#\\3${i}-${j}`);
+            HTMLcell.append(GameCell);
+        }
+    }
+}
