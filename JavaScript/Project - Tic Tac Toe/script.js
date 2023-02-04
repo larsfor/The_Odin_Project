af5e46e7afa3af9a8d78bd5fa2175e5406e1b127
Lars Fors <lars.forse@gmail.com>
2023-02-04 22:51:41 +0100

    Trying to get a game going. Not successfull.

--- JavaScript/Project - Tic Tac Toe/script.js
+++ JavaScript/Project - Tic Tac Toe/script.js
@@ -1,6 +1,53 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     console.log('DOMContent loaded')
+
+    
+    // // adding a click-event to get the  for the table <td>-cells
+    // let cells = document.querySelectorAll("td")
+    // cells.forEach((cell) => {
+    //     cell.addEventListener("click", () => {
+    //         // console.log(cell);
+    //         return cell;
+    //     })
+    // });
+    game = Game;
+    game.newGame();
 });
+
+const Game = ( () => {
+    const newGame = () => {
+        const player1 = Player('Lars', 'X');
+        const player2 = Player('Klars', 'O')
+        let cur_play = player1
+        while (true) {
+            // console.log('test');
+
+            // console.log(cur_play);
+
+            let player = getPlayer(cur_play);
+            let cur_play = player
+
+            console.log(cur_play);
+
+
+            if (gameOver) {
+                console.log('over');
+                break;
+            }
+        }
+    }
+    return { newGame }
+})();
+
+const getPlayer = ( (player) => {
+    let turn = (player == newGame.player1 ? newGame.player2 : newGame.player1);
+    return { turn };
+})
+
+const gameOver = ( () => {
+    let status = true;
+    return { status };
+})();
 
 // A player constructor as a factory
 const Player = (name, marker) => {
@@ -10,17 +57,23 @@ const Player = (name, marker) => {
 
 // A 3x3 array as a module
 const Gameboard = ( () => {
-    board = Array.from(Array(3), () => new Array(3, ''))
-    return { board }
+    let board = Array.from(Array(3), () => new Array(3, ''));
+    return { board };
 })();
 
-function generateTable() {
+// function generateTable() {
+//     gameboard = Gameboard;
+//     for (let i = 0; i < 3; i++) {
+//         for (let j = 0; j < 3; j++) {
+//             let GameCell = gameboard.board[i][j];
+//             let HTMLcell = document.querySelector(`#\\3${i}-${j}`);
+//             HTMLcell.append(GameCell);
+//         }
+//     }
+// }
+
+function placeMarker(cell, marker) {
+    
     gameboard = Gameboard;
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            let GameCell = gameboard.board[i][j];
-            let HTMLcell = document.querySelector(`#\\3${i}-${j}`);
-            HTMLcell.append(GameCell);
-        }
-    }
+
 }
