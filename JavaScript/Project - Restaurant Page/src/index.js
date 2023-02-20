b9ffcdb88acd15f4fd90eea785640385d3200fcb
Lars Fors <lars.forse@gmail.com>
2023-02-20 19:02:44 +0100

    Initial setup after some start struggles

--- JavaScript/Project - Restaurant Page/src/index.js
+++ JavaScript/Project - Restaurant Page/src/index.js
@@ -1,1 +1,19 @@
-console.log('test');
+import './style.css';
+import image from './restaurant.jpg';
+
+function component() {
+    const element = document.createElement('h1');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = 'Restaurant Page'
+    element.setAttribute("id", "content")
+
+    // Add the image to our existing div.
+    const myImage = new Image();
+    myImage.src = image;
+    element.appendChild(myImage);
+
+    return element;
+  }
+  
+  document.body.appendChild(component());
