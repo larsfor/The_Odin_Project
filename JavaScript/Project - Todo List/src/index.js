1b5afadef7acb43e558e2c8bfc04ddb7522259c2
Lars Fors <lars.forse@gmail.com>
2023-03-27 14:47:20 +0200

    added logic to show items in todolib using js

--- JavaScript/Project - Todo List/src/index.js
+++ JavaScript/Project - Todo List/src/index.js
@@ -5,18 +5,34 @@ import todoItem from './todoItem';
 document.addEventListener("DOMContentLoaded", (event) => {
     console.log('DOM content loaded');
 
+    // Refresh and re-generate the todo list
+    generateTodoList()
+
+    // Adding the action to add the todo item to the library
+    // when submitting the item.
     const todoForm = document.querySelector("#newTodoForm");
     todoForm.addEventListener("submit", (todo) => {
         let newTodo = createTodo(todoForm);
         addTodoToLibrary(newTodo);
-        console.log(todoLibrary);        
-
         todo.preventDefault();
     })
     event.preventDefault();
 })
 
-const todoLibrary = [];
+const todoLibrary = [1, 2];
+const gridContainer = document.querySelector(".grid-container");
+
+function generateTodoList() {
+    console.log('starting');
+    gridContainer.innerHTML = "";
+    console.log('list cleared');
+    todoLibrary.forEach((item) => {
+        let div = document.createElement("div");
+        div.classList.add("grid-item");
+        div.innerHTML = item;
+        gridContainer.appendChild(div);
+    })
+}
 
 function createTodo(form) {
     let title = form['title'].value;
@@ -25,6 +41,7 @@ function createTodo(form) {
     let priority = form['priority'].value;
 
     const todo = todoItem(title, description, dueDate, priority);
+    generateTodoList();
     return todo;
 }
 
