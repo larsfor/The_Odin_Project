36b735cfc28b7ac8f610ca031eede8f815fac184
Lars Fors <lars.forse@gmail.com>
2023-07-13 16:00:48 +0200

    Added password and password confirmation

--- JavaScript/Form Validation with JavaScript/Practice - Project/script.js
+++ JavaScript/Form Validation with JavaScript/Practice - Project/script.js
@@ -1,10 +1,14 @@
 const form = document.querySelector("form");
 const email = document.getElementById("email");
 const emailError = document.querySelector("#email + span.emailError");
-const country = document.getElementById("country")
-const countryError = document.querySelector("#country + span.countryError")
-const zipcode = document.getElementById("zipcode")
-const zipcodeError = document.querySelector("#zipcode + span.zipcodeError")
+const country = document.getElementById("country");
+const countryError = document.querySelector("#country + span.countryError");
+const zipcode = document.getElementById("zipcode");
+const zipcodeError = document.querySelector("#zipcode + span.zipcodeError");
+const password = document.getElementById("password");
+const passwordError = document.querySelector("#password + span.passwordError");
+const passwordConfirmation = document.getElementById("passwordConfirmation");
+const passwordConfirmationError = document.querySelector("#passwordConfirmation + span.passwordConfirmationError");
 
 form.addEventListener("submit", (e) => {
     if (!email.validity.valid) {
@@ -13,7 +17,13 @@ form.addEventListener("submit", (e) => {
     } else if (!country.validity.valid){
         showError();
         e.preventDefault();
-    }  else if (!zipcode.validity.valid) {
+    } else if (!zipcode.validity.valid) {
+        showError();
+        e.preventDefault();
+    } else if (!password.validity.valid) {
+        showError();
+        e.preventDefault();
+    } else if (passwordConfirmation.value != document.getElementById("password").value) {
         showError();
         e.preventDefault();
     }
@@ -37,6 +47,12 @@ zipcode.addEventListener("input", (e) => {
     }
 })
 
+passwordConfirmation.addEventListener("input", (e) => {
+    if (passwordConfirmation.value == document.getElementById("password").value) {
+        passwordConfirmationError.textContent = "";
+    }
+})
+
 function showError() {
     if (email.validity.valueMissing) {
         emailError.textContent = "Please enter email with the format x@y.z. Minmum length is 5."
@@ -44,11 +60,29 @@ function showError() {
         emailError.textContent = "Entered value needs to be an email address."
     } else if (email.validity.tooShort) {
         emailError.textContent = `Email should at least be ${email.minLength} characters; you entered ${email.value.length}.`;
-    } else if (country.validity.valueMissing) {
+    } 
+    
+    else if (country.validity.valueMissing) {
         countryError.textContent = "Please choose a country."
+    } 
+    
+    else if (zipcode.validity.valueMissing) {
+        zipcodeError.textContent = "Please enter a zip code."
     } else if (zipcode.validity.tooShort) {
         zipcodeError.textContent = "Zip Code must be at least 4 digits."
     } else if (zipcode.validity.tooLong) {
         zipcodeError.textContent = "Zip Code can't be longer than 9 digits."
+    } 
+
+    else if (password.validity.valueMissing) {
+        passwordError.textContent = "Please enter a password."
+    } else if (password.validity.patternMismatch) {
+        passwordError.textContent = "Password must at least contain one small letter, one large letter, a digit and a special character."
+    } else if (password.validity.tooShort) {
+        passwordError.textContent = "Password must be at least four characters long."
+    }
+
+    else if (passwordConfirmation.value !== document.getElementById("password").value) {
+        passwordConfirmationError.textContent = "Password confirmation needs to be equal to the password.";
     }
 }
