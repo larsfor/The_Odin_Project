5695d7ab9c54e9f034f52f45de735be6e949ce30
Lars Fors <lars.forse@gmail.com>
2023-07-21 21:08:36 +0200

    refactored the Ship factory and added tests

--- JavaScript/Project - Battlehip/src/ship.js
+++ JavaScript/Project - Battlehip/src/ship.js
@@ -1,11 +1,9 @@
 const Ship = (size) => {
   let hits = 0;
   const hit = () => hits += 1;
-  const isSunk = () => hits === size;
-
+  const isSunk = () => hits >= size;
+  
   return { hit, isSunk }
 } 
 
-let s = Ship(4);
-
-export { Ship };
+module.exports = Ship;
--- JavaScript/Project - Battlehip/src/ship.test.js
+++ JavaScript/Project - Battlehip/src/ship.test.js
@@ -1,4 +1,4 @@
-import { Ship } from './ship';
+const Ship = require('./ship');
 
 describe('Testing Ship factory', () => {
     test('Ship should be an object', () => {
@@ -12,12 +12,28 @@ describe('Testing Ship factory', () => {
     });
     test("A size 4 Ship shouldn't be sunk after 1 hit ", () => {
         let ship = Ship(4);
-        ship.hit()
+        ship.hit();
         expect(ship.isSunk()).toBe(false);
     });
     test("A size 1 Ship should be sunk after 1 hit ", () => {
         let ship = Ship(1);
-        ship.hit()
+        ship.hit();
+        expect(ship.isSunk()).toBe(true);
+    });
+    test("A size 4 Ship should be sunk after 4 hits ", () => {
+        let ship = Ship(4);
+        ship.hit();
+        ship.hit();
+        ship.hit();
+        ship.hit();
+        expect(ship.isSunk()).toBe(true);
+    });
+    test("A size 1 Ship should be sunk after 4 hits ", () => {
+        let ship = Ship(1);
+        ship.hit();
+        ship.hit();
+        ship.hit();
+        ship.hit();
         expect(ship.isSunk()).toBe(true);
     });
 })
