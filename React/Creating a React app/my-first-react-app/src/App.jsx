da90046c8003c1a5c806b2f26fe362555374deaa
Lars Fors <lars.forse@gmail.com>
2023-08-23 19:59:54 +0200

    tested the JSX element

--- React/Creating a React app/my-first-react-app/src/App.jsx
+++ React/Creating a React app/my-first-react-app/src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    hello world
-    </>
-  )
+    <div>
+      <h1>Test title</h1>
+      <ol className="test-list">
+        <li>List item 1</li>
+        <li>List item 2</li>
+        <li>List item 3</li>
+      </ol>
+      <svg >
+        <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+      </svg>
+      <form>
+        <input type="text" />
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- React/Creating a React app/my-first-react-app/src/main.jsx
+++ React/Creating a React app/my-first-react-app/src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import FoodEmoji from './FoodEmoji.jsx'
-import Greeting from './Greeting.jsx'
+// import FoodEmoji from './FoodEmoji.jsx'
+// import Greeting from './Greeting.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Greeting />
-    <FoodEmoji />
+    <App />
   </React.StrictMode>,
 )
