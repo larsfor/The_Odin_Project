38ef97db67d0db8c8f4629dfc0678cc340544563
larsfor <lars.forse@gmail.com>
2023-09-24 22:19:30 +0200

    Added a separate router component

--- React/React Router/react-router/src/main.jsx
+++ React/React Router/react-router/src/main.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from './App.jsx';
-import Profile from "./Profile";
-import ErrorPage from "./ErrorPage.jsx";
+import Router from "./Router.jsx";
 import './index.css';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/profile/:name",
-    element: <Profile />,
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router />
   </React.StrictMode>,
 )
