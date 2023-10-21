ccc114e22b00b34284695caddfca646c6b422c17
larsfor <21663232+larsfor@users.noreply.github.com>
2023-10-21 13:49:53 +0200

    added logic to show box on click

--- React/wheres_waldo/app/javascript/components/Home.jsx
+++ React/wheres_waldo/app/javascript/components/Home.jsx
@@ -8,7 +8,27 @@ export default function Home() {
   const [position, setPosition] = useState({
     x: 0,
     y: 0
-  })
+  });
+  const [clicked, setClicked] = useState(false);
+  const [boxPosition, setBoxPosition] = useState({
+    x: 0,
+    y: 0
+  });
+
+  function handleClick() {
+    setBoxPosition({
+      x: position.x,
+      y: position.y
+    })
+    const box = document.getElementById("targetBox");
+    if (clicked) {
+      box.style.display = 'none';
+    } else if (!clicked) {
+      box.style.display = 'block';
+    }
+    setClicked(!clicked);
+  }
+
 
   return (
     <>
@@ -20,7 +40,25 @@ export default function Home() {
           })
         }}
         
+        onClick={handleClick}
         >
+
+        <div 
+          id="targetBox"
+          style={{
+            display: 'none',
+            position: 'absolute',
+            transform: `translate(${boxPosition.x}px, ${boxPosition.y}px)`,
+            left: -20,
+            top: -20,
+            width: 40,
+            height: 40,
+            borderStyle: 'dashed',
+            maskBorderWidth: "1px"
+          }}
+        >
+
+        </div>
         <Row>
             <Image src="https://images.firstpost.com/wp-content/uploads/2018/04/Wheres-waldo-wally-google-maps-380.png" rounded />
         </Row>
