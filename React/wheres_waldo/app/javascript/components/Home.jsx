ade440b39fcfc0f7042bb3c10cf3ee758bd6aca4
larsfor <21663232+larsfor@users.noreply.github.com>
2023-10-21 13:26:31 +0200

    added position as a state

--- React/wheres_waldo/app/javascript/components/Home.jsx
+++ React/wheres_waldo/app/javascript/components/Home.jsx
@@ -1,16 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
+import { useState } from "react";
 
 export default function Home() {
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0
+  })
+
   return (
-    <Container>
-      <Row>
-          <Image src="https://images.firstpost.com/wp-content/uploads/2018/04/Wheres-waldo-wally-google-maps-380.png" rounded />
-      </Row>
-    </Container>
+    <>
+      <Container
+        onPointerMove={e => {
+          setPosition({
+            x: e.clientX,
+            y: e.clientY
+          })
+        }}
+        
+        >
+        <Row>
+            <Image src="https://images.firstpost.com/wp-content/uploads/2018/04/Wheres-waldo-wally-google-maps-380.png" rounded />
+        </Row>
+      </Container>
+    </>
   );
 }
